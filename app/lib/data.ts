f89087d4129d5bd1b4fd618bb154a101c62cc716
uthayasakthivel <uthayasakthivel8@gmail.com>
2025-03-28 22:21:01 +0530

    Versioning untracked files

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -2,9 +2,9 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import TodoGlassmorphism from "../../public/TodoGlassmorphism.png";
-import ultimateCookBook from "../../public/ultimateCookBook.png";
-import travelAdvisor from "../../public/travelAdvisor.png";
+import TodoGlassmorphism from "/public/TodoGlassmorphism.png";
+import ultimateCookBook from "/public/ultimateCookBook.png";
+import travelAdvisor from "/public/travelAdvisor.png";
 
 export const links = [
   {
